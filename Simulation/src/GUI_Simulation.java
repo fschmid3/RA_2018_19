29a74de9941a5ebbf35ae76c8b2b3c910d87483e
student <student@192.168.101.20>
2018-10-11 10:32:31 +0200

    Initial Commit

---
+++ Simulation/src/GUI_Simulation.java
@@ -0,0 +1,41 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class GUI_Simulation {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUI_Simulation window = new GUI_Simulation();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public GUI_Simulation() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 730, 512);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
