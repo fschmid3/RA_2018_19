623421ba4e75c28157b7014c62543e5e2121b9d6
student <student@192.168.101.20>
2018-10-11 12:53:45 +0200

    parser in Parser.java verschoben

--- Simulation/src/Steuerung/Parser.java
+++ Simulation/src/Steuerung/Parser.java
@@ -1,5 +1,55 @@
 package Steuerung;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class Parser {
-	
+
+
+	public String [] readFile(String path) throws IOException{	
+		FileReader fReader = null;
+
+		//String path = "testfiles/TPicSim1.LST";
+
+		fReader = new FileReader(path);
+
+		char [] acBuffer = new char [9];
+		BufferedReader bReader = new BufferedReader(fReader);
+
+		String [] fileContent = new String[256];
+		String lineBuffer = " ";
+
+		lineBuffer = bReader.readLine();
+
+		for (int indexContent = 0; lineBuffer != null; indexContent++) {
+			fileContent[indexContent] = lineBuffer;
+			lineBuffer = bReader.readLine();
+		}
+		return fileContent;
+	}
+
+	public int[] parseCode(String[] fileContent) throws IOException{	
+
+		//String path = "testfiles/TPicSim1.LST";
+
+		int [] aiBuffer = new int [50];
+		String codeBuffer = null;
+		int indexContent;
+		int i= 0;
+		for(indexContent = 0; (!fileContent[indexContent].contains("start")) && (!fileContent[indexContent].contains("loop")); indexContent++) {}
+		indexContent++;
+		for(;!fileContent[indexContent].contains("ende"); indexContent++) {
+			
+			codeBuffer= fileContent[indexContent].substring(5, 10);
+			if(!codeBuffer.contains(" ")) {
+				aiBuffer[i] = Integer.parseInt(codeBuffer,16);
+			}
+		}
+		return aiBuffer;
+	}
 }
+
+
+
--- Simulation/src/main.java
+++ Simulation/src/main.java
@@ -1,40 +1,9 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Arrays;
+
 
 public class main {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		FileReader fReader = null;
 		
-		String path = "testfiles/TPicSim1.LST";
-		
-		fReader = new FileReader(path);
-
-		char [] acBuffer = new char [9];
-		BufferedReader bReader = new BufferedReader(fReader);
-		
-		String [] fileContent = new String[256];
-		String lineBuffer = " ";
-		int indexContent;
-
-		for(indexContent = 0; (!lineBuffer.contains("start")) && (!lineBuffer.contains("loop")); indexContent++) {
-			
-			lineBuffer = bReader.readLine();
-			fileContent[indexContent] = lineBuffer;
-			;
-		}
-		
-		for(;!lineBuffer.contains("ende"); indexContent++) {
-			
-			if(bReader.read(acBuffer, 0, 9)!= -1) {
-				System.out.println(Arrays.toString(acBuffer));
-			}
-			lineBuffer = bReader.readLine();
-
-			fileContent[indexContent] = lineBuffer;
-		}
 	}
 }
