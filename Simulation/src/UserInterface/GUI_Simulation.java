a11389f302013f050c4e983e8f4d731c573ce893
fschmid3 <friedrich.schmidt94@gmail.com>
2018-12-13 12:35:06 +0100

    Gui etwas verändert

--- Simulation/src/UserInterface/GUI_Simulation.java
+++ Simulation/src/UserInterface/GUI_Simulation.java
@@ -91,6 +91,7 @@ public class GUI_Simulation {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.setResizable(false);
 		frame.getContentPane().setBackground(SystemColor.inactiveCaptionBorder);
 		frame.getContentPane().addMouseListener(new MouseAdapter() {
 			@Override
@@ -113,152 +114,152 @@ public class GUI_Simulation {
 				timer_txt = new JTextField();
 				timer_txt.setEditable(false);
 				timer_txt.setColumns(10);
-				timer_txt.setBounds(96, 478, 63, 22);
+				timer_txt.setBounds(96, 569, 63, 22);
 				frame.getContentPane().add(timer_txt);
 				
 				JLabel timer_lbl = new JLabel("Timer0");
-				timer_lbl.setBounds(34, 481, 56, 16);
+				timer_lbl.setBounds(34, 572, 56, 16);
 				frame.getContentPane().add(timer_lbl);
 		
 				JLabel lblC = new JLabel("C");
-				lblC.setBounds(545, 574, 20, 16);
+				lblC.setBounds(910, 543, 20, 16);
 				frame.getContentPane().add(lblC);
 		
 		JLabel lblRp = new JLabel("RP0");
-		lblRp.setBounds(431, 574, 30, 16);
+		lblRp.setBounds(796, 543, 30, 16);
 		frame.getContentPane().add(lblRp);
 		
 				JLabel lblZ = new JLabel("Z");
-				lblZ.setBounds(499, 574, 20, 16);
+				lblZ.setBounds(864, 543, 20, 16);
 				frame.getContentPane().add(lblZ);
 		
 				textField_10 = new JTextField();
 				textField_10.setEditable(false);
 				textField_10.setColumns(10);
-				textField_10.setBounds(541, 600, 20, 22);
+				textField_10.setBounds(906, 569, 20, 22);
 				frame.getContentPane().add(textField_10);
 		
 		JLabel lblpd = new JLabel("!PD");
-		lblpd.setBounds(475, 574, 20, 16);
+		lblpd.setBounds(840, 543, 20, 16);
 		frame.getContentPane().add(lblpd);
 		
 		JLabel lblto = new JLabel("!TO");
-		lblto.setBounds(453, 574, 30, 16);
+		lblto.setBounds(818, 543, 30, 16);
 		frame.getContentPane().add(lblto);
 		
 				txtPD = new JTextField();
 				txtPD.setEditable(false);
 				txtPD.setColumns(10);
-				txtPD.setBounds(475, 600, 20, 22);
+				txtPD.setBounds(840, 569, 20, 22);
 				frame.getContentPane().add(txtPD);
 		
 		txtTO = new JTextField();
 		txtTO.setEditable(false);
 		txtTO.setColumns(10);
-		txtTO.setBounds(453, 600, 20, 22);
+		txtTO.setBounds(818, 569, 20, 22);
 		frame.getContentPane().add(txtTO);
 		
 		JLabel lblIrp = new JLabel("IRP");
-		lblIrp.setBounds(387, 574, 20, 16);
+		lblIrp.setBounds(752, 543, 20, 16);
 		frame.getContentPane().add(lblIrp);
 		
 				textField_9 = new JTextField();
 				textField_9.setEditable(false);
 				textField_9.setColumns(10);
-				textField_9.setBounds(519, 600, 20, 22);
+				textField_9.setBounds(884, 569, 20, 22);
 				frame.getContentPane().add(textField_9);
 		
 		txtIRP = new JTextField();
 		txtIRP.setEditable(false);
 		txtIRP.setColumns(10);
-		txtIRP.setBounds(387, 600, 20, 22);
+		txtIRP.setBounds(752, 569, 20, 22);
 		frame.getContentPane().add(txtIRP);
 		
 				textField_8 = new JTextField();
 				textField_8.setEditable(false);
 				textField_8.setColumns(10);
-				textField_8.setBounds(497, 600, 20, 22);
+				textField_8.setBounds(862, 569, 20, 22);
 				frame.getContentPane().add(textField_8);
 		
 				JLabel lblStatus = new JLabel("Status");
-				lblStatus.setBounds(329, 603, 56, 16);
+				lblStatus.setBounds(694, 572, 56, 16);
 				frame.getContentPane().add(lblStatus);
 		
 				JLabel lblDc = new JLabel("DC");
-				lblDc.setBounds(522, 574, 20, 16);
+				lblDc.setBounds(887, 543, 20, 16);
 				frame.getContentPane().add(lblDc);
 		
 		txtRP0 = new JTextField();
 		txtRP0.setEditable(false);
 		txtRP0.setColumns(10);
-		txtRP0.setBounds(431, 600, 20, 22);
+		txtRP0.setBounds(796, 569, 20, 22);
 		frame.getContentPane().add(txtRP0);
 		
 		JLabel lblRp_1 = new JLabel("RP1");
-		lblRp_1.setBounds(409, 574, 30, 16);
+		lblRp_1.setBounds(774, 543, 30, 16);
 		frame.getContentPane().add(lblRp_1);
 		
 		txtRP1 = new JTextField();
 		txtRP1.setEditable(false);
 		txtRP1.setColumns(10);
-		txtRP1.setBounds(409, 600, 20, 22);
+		txtRP1.setBounds(774, 569, 20, 22);
 		frame.getContentPane().add(txtRP1);
-		textField_1.setBounds(673, 211, 20, 22);
+		textField_1.setBounds(770, 211, 20, 22);
 		frame.getContentPane().add(textField_1);
 		textField_1.setColumns(10);
 
 		JLabel lblPortB = new JLabel("Port B");
-		lblPortB.setBounds(673, 178, 56, 16);
+		lblPortB.setBounds(770, 178, 56, 16);
 		frame.getContentPane().add(lblPortB);
 
 		JLabel lblW = new JLabel("w");
-		lblW.setBounds(673, 289, 56, 16);
+		lblW.setBounds(770, 289, 56, 16);
 		frame.getContentPane().add(lblW);
 
 		JLabel lblPc = new JLabel("PC");
-		lblPc.setBounds(673, 318, 56, 16);
+		lblPc.setBounds(770, 318, 56, 16);
 		frame.getContentPane().add(lblPc);
 
 		JLabel lblNewLabel = new JLabel("FSR");
-		lblNewLabel.setBounds(673, 347, 56, 16);
+		lblNewLabel.setBounds(770, 347, 56, 16);
 		frame.getContentPane().add(lblNewLabel);
 
 		JLabel lblPcl = new JLabel("PCL");
-		lblPcl.setBounds(673, 376, 56, 16);
+		lblPcl.setBounds(770, 376, 56, 16);
 		frame.getContentPane().add(lblPcl);
 
 		JLabel lblPclatch = new JLabel("PCLATCH");
-		lblPclatch.setBounds(673, 405, 56, 16);
+		lblPclatch.setBounds(770, 405, 56, 16);
 		frame.getContentPane().add(lblPclatch);
 
 		textField_2 = new JTextField();
 		textField_2.setEditable(false);
-		textField_2.setBounds(741, 286, 63, 22);
+		textField_2.setBounds(838, 286, 63, 22);
 		frame.getContentPane().add(textField_2);
 		textField_2.setColumns(10);
 
 		textField_3 = new JTextField();
 		textField_3.setEditable(false);
 		textField_3.setColumns(10);
-		textField_3.setBounds(741, 315, 63, 22);
+		textField_3.setBounds(838, 315, 63, 22);
 		frame.getContentPane().add(textField_3);
 
 		textField_4 = new JTextField();
 		textField_4.setEditable(false);
 		textField_4.setColumns(10);
-		textField_4.setBounds(741, 344, 63, 22);
+		textField_4.setBounds(838, 344, 63, 22);
 		frame.getContentPane().add(textField_4);
 
 		textField_5 = new JTextField();
 		textField_5.setEditable(false);
 		textField_5.setColumns(10);
-		textField_5.setBounds(741, 373, 63, 22);
+		textField_5.setBounds(838, 373, 63, 22);
 		frame.getContentPane().add(textField_5);
 
 		textField_6 = new JTextField();
 		textField_6.setEditable(false);
 		textField_6.setColumns(10);
-		textField_6.setBounds(741, 402, 63, 22);
+		textField_6.setBounds(838, 402, 63, 22);
 		frame.getContentPane().add(textField_6);
 
 		textField_11 = new JTextField();
@@ -270,7 +271,7 @@ public class GUI_Simulation {
 			}
 		});
 		textField_11.setColumns(10);
-		textField_11.setBounds(693, 211, 20, 22);
+		textField_11.setBounds(790, 211, 20, 22);
 		frame.getContentPane().add(textField_11);
 
 		textField_12 = new JTextField();
@@ -282,7 +283,7 @@ public class GUI_Simulation {
 			}
 		});
 		textField_12.setColumns(10);
-		textField_12.setBounds(713, 211, 20, 22);
+		textField_12.setBounds(810, 211, 20, 22);
 		frame.getContentPane().add(textField_12);
 
 		textField_13 = new JTextField();
@@ -294,7 +295,7 @@ public class GUI_Simulation {
 			}
 		});
 		textField_13.setColumns(10);
-		textField_13.setBounds(733, 211, 20, 22);
+		textField_13.setBounds(830, 211, 20, 22);
 		frame.getContentPane().add(textField_13);
 
 		textField_14 = new JTextField();
@@ -306,7 +307,7 @@ public class GUI_Simulation {
 			}
 		});
 		textField_14.setColumns(10);
-		textField_14.setBounds(753, 211, 20, 22);
+		textField_14.setBounds(850, 211, 20, 22);
 		frame.getContentPane().add(textField_14);
 
 		textField_15 = new JTextField();
@@ -318,7 +319,7 @@ public class GUI_Simulation {
 			}
 		});
 		textField_15.setColumns(10);
-		textField_15.setBounds(773, 211, 20, 22);
+		textField_15.setBounds(870, 211, 20, 22);
 		frame.getContentPane().add(textField_15);
 
 		textField_16 = new JTextField();
@@ -330,35 +331,35 @@ public class GUI_Simulation {
 			}
 		});
 		textField_16.setColumns(10);
-		textField_16.setBounds(793, 211, 20, 22);
+		textField_16.setBounds(890, 211, 20, 22);
 		frame.getContentPane().add(textField_16);
 
 		JLabel label = new JLabel("7");
-		label.setBounds(677, 194, 20, 16);
+		label.setBounds(774, 194, 20, 16);
 		frame.getContentPane().add(label);
 
 		JLabel label_1 = new JLabel("6");
-		label_1.setBounds(693, 194, 20, 16);
+		label_1.setBounds(790, 194, 20, 16);
 		frame.getContentPane().add(label_1);
 
 		JLabel label_2 = new JLabel("5");
-		label_2.setBounds(713, 193, 20, 16);
+		label_2.setBounds(810, 193, 20, 16);
 		frame.getContentPane().add(label_2);
 
 		JLabel label_3 = new JLabel("4");
-		label_3.setBounds(733, 194, 20, 16);
+		label_3.setBounds(830, 194, 20, 16);
 		frame.getContentPane().add(label_3);
 
 		JLabel label_4 = new JLabel("3");
-		label_4.setBounds(753, 194, 20, 16);
+		label_4.setBounds(850, 194, 20, 16);
 		frame.getContentPane().add(label_4);
 
 		JLabel label_5 = new JLabel("2");
-		label_5.setBounds(773, 195, 20, 16);
+		label_5.setBounds(870, 195, 20, 16);
 		frame.getContentPane().add(label_5);
 
 		JLabel label_6 = new JLabel("1");
-		label_6.setBounds(793, 195, 20, 16);
+		label_6.setBounds(890, 195, 20, 16);
 		frame.getContentPane().add(label_6);
 
 		textField_17 = new JTextField();
@@ -370,47 +371,47 @@ public class GUI_Simulation {
 			}
 		});
 		textField_17.setColumns(10);
-		textField_17.setBounds(813, 211, 20, 22);
+		textField_17.setBounds(910, 211, 20, 22);
 		frame.getContentPane().add(textField_17);
 
 		JLabel label_7 = new JLabel("0");
-		label_7.setBounds(813, 195, 20, 16);
+		label_7.setBounds(910, 195, 20, 16);
 		frame.getContentPane().add(label_7);
 
 		JLabel lblPortA = new JLabel("Port A");
-		lblPortA.setBounds(673, 99, 56, 16);
+		lblPortA.setBounds(770, 99, 56, 16);
 		frame.getContentPane().add(lblPortA);
 
 		JLabel label_9 = new JLabel("7");
-		label_9.setBounds(677, 115, 20, 16);
+		label_9.setBounds(774, 115, 20, 16);
 		frame.getContentPane().add(label_9);
 
 		JLabel label_10 = new JLabel("6");
-		label_10.setBounds(693, 115, 20, 16);
+		label_10.setBounds(790, 115, 20, 16);
 		frame.getContentPane().add(label_10);
 
 		JLabel label_11 = new JLabel("5");
-		label_11.setBounds(713, 115, 20, 16);
+		label_11.setBounds(810, 115, 20, 16);
 		frame.getContentPane().add(label_11);
 
 		JLabel label_12 = new JLabel("4");
-		label_12.setBounds(733, 115, 20, 16);
+		label_12.setBounds(830, 115, 20, 16);
 		frame.getContentPane().add(label_12);
 
 		JLabel label_13 = new JLabel("3");
-		label_13.setBounds(753, 115, 20, 16);
+		label_13.setBounds(850, 115, 20, 16);
 		frame.getContentPane().add(label_13);
 
 		JLabel label_14 = new JLabel("2");
-		label_14.setBounds(773, 116, 20, 16);
+		label_14.setBounds(870, 116, 20, 16);
 		frame.getContentPane().add(label_14);
 
 		JLabel label_15 = new JLabel("1");
-		label_15.setBounds(793, 116, 20, 16);
+		label_15.setBounds(890, 116, 20, 16);
 		frame.getContentPane().add(label_15);
 
 		JLabel label_16 = new JLabel("0");
-		label_16.setBounds(813, 116, 20, 16);
+		label_16.setBounds(910, 116, 20, 16);
 		frame.getContentPane().add(label_16);
 
 		textField_18 = new JTextField();
@@ -422,7 +423,7 @@ public class GUI_Simulation {
 			}
 		});
 		textField_18.setColumns(10);
-		textField_18.setBounds(813, 132, 20, 22);
+		textField_18.setBounds(910, 132, 20, 22);
 		frame.getContentPane().add(textField_18);
 
 		textField_19 = new JTextField();
@@ -434,7 +435,7 @@ public class GUI_Simulation {
 			}
 		});
 		textField_19.setColumns(10);
-		textField_19.setBounds(793, 132, 20, 22);
+		textField_19.setBounds(890, 132, 20, 22);
 		frame.getContentPane().add(textField_19);
 
 		textField_20 = new JTextField();
@@ -446,7 +447,7 @@ public class GUI_Simulation {
 			}
 		});
 		textField_20.setColumns(10);
-		textField_20.setBounds(773, 132, 20, 22);
+		textField_20.setBounds(870, 132, 20, 22);
 		frame.getContentPane().add(textField_20);
 
 		textField_21 = new JTextField();
@@ -458,7 +459,7 @@ public class GUI_Simulation {
 			}
 		});
 		textField_21.setColumns(10);
-		textField_21.setBounds(753, 132, 20, 22);
+		textField_21.setBounds(850, 132, 20, 22);
 		frame.getContentPane().add(textField_21);
 
 		textField_22 = new JTextField();
@@ -470,7 +471,7 @@ public class GUI_Simulation {
 			}
 		});
 		textField_22.setColumns(10);
-		textField_22.setBounds(733, 132, 20, 22);
+		textField_22.setBounds(830, 132, 20, 22);
 		frame.getContentPane().add(textField_22);
 
 		textField_23 = new JTextField();
@@ -482,7 +483,7 @@ public class GUI_Simulation {
 			}
 		});
 		textField_23.setColumns(10);
-		textField_23.setBounds(713, 132, 20, 22);
+		textField_23.setBounds(810, 132, 20, 22);
 		frame.getContentPane().add(textField_23);
 
 		textField_24 = new JTextField();
@@ -494,7 +495,7 @@ public class GUI_Simulation {
 			}
 		});
 		textField_24.setColumns(10);
-		textField_24.setBounds(693, 132, 20, 22);
+		textField_24.setBounds(790, 132, 20, 22);
 		frame.getContentPane().add(textField_24);
 
 		textField_25 = new JTextField();
@@ -506,7 +507,7 @@ public class GUI_Simulation {
 			}
 		});
 		textField_25.setColumns(10);
-		textField_25.setBounds(673, 132, 20, 22);
+		textField_25.setBounds(770, 132, 20, 22);
 		frame.getContentPane().add(textField_25);
 
 		JButton btnRun = new JButton("Run");
@@ -548,7 +549,7 @@ public class GUI_Simulation {
 		table = new JTable();
 		
 		
-		table.setFont(new Font("Courier New", Font.PLAIN, 13));
+		table.setFont(table.getFont().deriveFont(12f));
 		table.setModel(new DefaultTableModel(new Object[350][3], new String[] {"M", "B", "New column"}) {
 			boolean[] columnEditables = new boolean[] {
 					false, false, false
@@ -572,7 +573,7 @@ public class GUI_Simulation {
 		frame.getContentPane().add(table);
 
 		JScrollPane scrollPane = new JScrollPane(table);
-		scrollPane.setBounds(34, 99, 531, 351);
+		scrollPane.setBounds(34, 99, 715, 457);
 		frame.getContentPane().add(scrollPane);
 		
 		table.addMouseListener(new MouseAdapter() {
@@ -612,23 +613,23 @@ public class GUI_Simulation {
 		frame.getContentPane().add(btnLoadLstFile);
 		
 		JLabel intcon_lbl = new JLabel("INTCON");
-		intcon_lbl.setBounds(673, 463, 56, 16);
+		intcon_lbl.setBounds(770, 463, 56, 16);
 		frame.getContentPane().add(intcon_lbl);
 		
 		intcon_txt = new JTextField();
 		intcon_txt.setEditable(false);
 		intcon_txt.setColumns(10);
-		intcon_txt.setBounds(741, 460, 63, 22);
+		intcon_txt.setBounds(838, 460, 63, 22);
 		frame.getContentPane().add(intcon_txt);
 		
 		JLabel optionReg_lbl = new JLabel("option");
-		optionReg_lbl.setBounds(673, 492, 56, 16);
+		optionReg_lbl.setBounds(770, 492, 56, 16);
 		frame.getContentPane().add(optionReg_lbl);
 		
 		optionReg_txt = new JTextField();
 		optionReg_txt.setEditable(false);
 		optionReg_txt.setColumns(10);
-		optionReg_txt.setBounds(741, 489, 63, 22);
+		optionReg_txt.setBounds(838, 489, 63, 22);
 		frame.getContentPane().add(optionReg_txt);
 	}
 
